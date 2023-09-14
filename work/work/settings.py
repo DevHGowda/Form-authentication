b5652e886143f36089f4da79fec0953b1e81f560
DEV H GOWDA <113254147+DevHGowda@users.noreply.github.com>
2023-09-14 16:58:50 +0530

    Update settings.py

--- work/work/settings.py
+++ work/work/settings.py
@@ -82,7 +82,7 @@ DATABASES = {
         "ENGINE": "django.db.backends.mysql",
         "NAME": "quizz",
         "USER": "root",
-        "PASSWORD": "Dev@26.08.2002",
+        "PASSWORD": "",
         "HOST": "127.0.0.1",
         "PORT": "3307",
     }
